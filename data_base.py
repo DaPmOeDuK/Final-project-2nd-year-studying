dbbee8bbd9cc56aa91904dae5536a145dde28341
DaPmOeDuK <pogorelovgrigorij6@gmail.com>
2024-12-02 18:05:12 +0300

    first preparations for project

---
+++ data_base.py
@@ -0,0 +1,7 @@
+import s_taper as s_t
+from s_taper.consts import *
+
+base = {
+    "user_id": INT + KEY
+
+}
---
+++ main.py
@@ -0,0 +1,4 @@
+import telebot
+from telebot.types import Message, ReplyKeyboardMarkup as RKM, ReplyKeyboardRemove as RKR
+from telebot.types import InlineKeyboardMarkup as IKM, InlineKeyboardButton as IKB, CallbackQuery
+from config import TOKEN
