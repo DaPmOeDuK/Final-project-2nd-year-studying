ca483a186cf737bfc39df45f9677273a83765be9
DaPmOeDuK <pogorelovgrigorij6@gmail.com>
2024-12-04 17:49:52 +0300

    first preparations for project

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import telebot
 from telebot.types import Message, ReplyKeyboardMarkup as RKM, ReplyKeyboardRemove as RKR
 from telebot.types import InlineKeyboardMarkup as IKM, InlineKeyboardButton as IKB, CallbackQuery
 from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+temp = {}
+clear = RKRe()
