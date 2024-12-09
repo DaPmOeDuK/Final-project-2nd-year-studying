9bdb8b3a60ce5611c9716c33e66ad24b27c6d51a
DaPmOeDuK <pogorelovgrigorij6@gmail.com>
2024-12-09 16:01:36 +0300

    make new funcs: start, reg. Delete data_base.py

--- data_base.py
+++
@@ -1,9 +0,0 @@
-import s_taper as s_t
-from s_taper.consts import *
-
-base = {
-    "user_id": INT + KEY
-
-}
-
-users = s_t.Taper("users", "data.db").create_table(base)
--- main.py
+++ main.py
@@ -3,7 +3,36 @@ from telebot.types import Message, ReplyKeyboardMarkup as RKM, ReplyKeyboardRemo
 from telebot.types import InlineKeyboardMarkup as IKM, InlineKeyboardButton as IKB, CallbackQuery
 from config import TOKEN
 
-
 bot = telebot.TeleBot(TOKEN)
-temp = {}
-clear = RKRe()
+clear = RKR()
+
+
+@bot.message_handler(commands=["start"])
+def start(m: Message):
+    kb = RKM(True, True)
+    kb.row("Хрустальный шар", 'Книга "Гадаем на кофейной гуще"')
+    kb.row("Магический шар", "Руны")
+    bot.send_message(m.chat.id, 'Здравствуйте, магазин эзотерики "Шары из Гусь-Хрустального" приветствует Вас',
+                     reply_markup=kb)
+    bot.register_next_step_handler(m, reg,kb)
+
+
+def reg(m:Message, kb:RKM):
+    if m.text == "Хрустальный шар":
+        hr_shar(m)
+        return
+    elif m.text == 'Книга "Гадаем на кофейной гуще"':
+        book(m)
+        return
+    elif m.text == "Магический шар":
+        mag_shar(m)
+        return
+    elif m.text == "Руны":
+        runes(m)
+        return
+    else:
+        bot.send_message(m.chat.id, "Такого товара нет, пожалуйста выберите товар, нажав на кнопку снизу",
+                         reply_markup=kb)
+        bot.register_next_step_handler(m, reg,kb)
+
+bot.infinity_polling()
